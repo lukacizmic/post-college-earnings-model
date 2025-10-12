fbca0bff77bde67f170421675a1405d25e10af39
Luka Cizmic <845a4947-649b-4386-9644-ebb7d08f2f30@4146c987a9a6>
2025-10-12 19:05:55 +0000

    Initial commit with model

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+model = joblib.load("college_app/fitted_random_forest_pipeline.joblib")
+
+df = pd.read_csv("my_data.csv")
+
+college = st.text_input("College")
+region = st.text_input("Region")
+
+if st.button("Predict"):
+    input_data = pd.DataFrame({
+        "college": [college],
+        "region": [region]
+    })
+    prediction = model.predict(input_data)
+    st.success(f" Estimated earnings: ${prediction[0]:,.2f} per year")
