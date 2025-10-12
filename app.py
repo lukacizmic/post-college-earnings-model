a3ca6a5e9734f1a08a290957e1790a4854ec079c
Luka Cizmic <cizmalu@gmail.com>
2025-10-12 21:41:45 +0200

    fix: remove data frame

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import pandas as pd
 
 model = joblib.load("fitted_random_forest_pipeline.joblib")
 
-df = pd.read_csv("my_data.csv")
-
 college = st.text_input("College")
 region = st.text_input("Region")
 
